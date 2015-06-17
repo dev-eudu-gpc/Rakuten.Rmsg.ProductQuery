a2dc7472697af612d7916a3773407178d038c603
wabirch <wayne.birch@play.com>
2015-06-17 11:17:51 +0100

    Implemented IDisposable on WebJobClient

--- src/Rakuten.Rmsg.ProductQuery.Web.Http.Tests.Integration/WebJobClient/WebJobClient.cs
+++ src/Rakuten.Rmsg.ProductQuery.Web.Http.Tests.Integration/WebJobClient/WebJobClient.cs
@@ -13,42 +13,8 @@ namespace Rakuten.Rmsg.ProductQuery.Web.Http.Tests.Integration
     /// <summary>
     /// A client for interacting with the web job.
     /// </summary>
-    public class WebJobClient
+    public class WebJobClient : IDisposable
     {
-        /// <summary>
-        /// The process for the web job.
-        /// </summary>
-        private readonly Process process;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WebJobClient"/> class
-        /// </summary>
-        public WebJobClient()
-        {
-            // Configure the process
-            this.process = new Process();
-
-#if DEBUG
-            var environment = "Debug";
-#else
-            var environment = "Release";
-#endif
-
-            this.process.StartInfo.FileName = Directory.GetCurrentDirectory() + string.Format("\\..\\..\\..\\Rakuten.Rmsg.ProductQuery.WebJob\\bin\\{0}\\Rakuten.Rmsg.ProductQuery.WebJob.exe", environment);
-            this.process.StartInfo.RedirectStandardError = true;
-            this.process.StartInfo.RedirectStandardOutput = true;
-            this.process.StartInfo.UseShellExecute = false;
-            this.process.ErrorDataReceived += (sender, line) =>
-            {
-                this.HasErrors = true;
-            };
-            this.process.OutputDataReceived += (sender, line) =>
-            {
-                // The web job is considered started when specific text is written to standard output
-                this.IsReady = line.Data.Equals("Job host started", StringComparison.InvariantCultureIgnoreCase);
-            };
-        }
-
         /// <summary>
         /// Gets a value indicating whether the web job threw exceptions
         /// when starting up.
@@ -61,14 +27,37 @@ namespace Rakuten.Rmsg.ProductQuery.Web.Http.Tests.Integration
         public bool IsReady { get; private set; }
 
         /// <summary>
+        /// Gets or sets the process for the web job.
+        /// </summary>
+        private Process Process { get; set; }
+
+        /// <summary>
         /// Starts the web job.
         /// </summary>
         public void Start()
         {
-            this.process.Start();
+            // Ensure any previous running process has been stopped and disposed.
+            this.Stop();
 
-            this.process.BeginOutputReadLine();
-            this.process.BeginErrorReadLine();
+            // Configure the process
+            this.Process = new Process();
+
+#if DEBUG
+            var environment = "Debug";
+#else
+            var environment = "Release";
+#endif
+
+            this.Process.StartInfo.FileName = Directory.GetCurrentDirectory() + string.Format("\\..\\..\\..\\Rakuten.Rmsg.ProductQuery.WebJob\\bin\\{0}\\Rakuten.Rmsg.ProductQuery.WebJob.exe", environment);
+            this.Process.StartInfo.RedirectStandardError = true;
+            this.Process.StartInfo.RedirectStandardOutput = true;
+            this.Process.StartInfo.UseShellExecute = false;
+            this.Process.ErrorDataReceived += this.ErrorDataReceivedHandler;
+            this.Process.OutputDataReceived += this.OutputDataReceivedHandler;
+            this.Process.Start();
+
+            this.Process.BeginOutputReadLine();
+            this.Process.BeginErrorReadLine();
 
             // Wait until the process is considered ready or errors have been thrown
             while (!this.IsReady || this.HasErrors)
@@ -81,7 +70,39 @@ namespace Rakuten.Rmsg.ProductQuery.Web.Http.Tests.Integration
         /// </summary>
         public void Stop()
         {
-            this.process.Close();
+            if (this.Process != null)
+            {
+                this.Process.Close();
+                this.Process.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Disposal procedures.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Process.Kill();
+        }
+
+        /// <summary>
+        /// Handlers for the error data received event.
+        /// </summary>
+        /// <param name="sender">The sender of the error.</param>
+        /// <param name="e">Event arguments</param>
+        private void ErrorDataReceivedHandler(object sender, DataReceivedEventArgs e)
+        {
+            this.HasErrors = true;
+        }
+
+        /// <summary>
+        /// Handler for the output data received event.
+        /// </summary>
+        /// <param name="sender">The sender of the error.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OutputDataReceivedHandler(object sender, DataReceivedEventArgs e)
+        {
+            this.IsReady = this.IsReady || e.Data.Equals("Job host started", StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
